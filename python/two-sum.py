bdb7e8d1af92b9321d9feb1135ffb863579cb923
Anirudh678 <anirudhnadig3@gmail.com>
2025-08-12 16:23:07 -0400

    adding two sum

---
+++ python/two-sum.py
@@ -0,0 +1,15 @@
+from typing import List   #we import list like this
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        seen = {}
+
+        for i, num in enumerate(nums):
+
+            comp = target - num
+
+            if comp in seen:
+                return [seen[comp], i]
+            seen[num] = i
